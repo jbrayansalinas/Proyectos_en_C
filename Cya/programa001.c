06c6c39c382a5063dc152ef010a6d5736baf54ef
jbrayansalinas <100964746+3115111872@users.noreply.github.com>
2022-08-17 15:25:17 -0500

    Proyecto 1 y 2

---
+++ Cya/programa001.c
@@ -0,0 +1,17 @@
+/*#include<stdio.h>
+
+int main(){
+
+    int horasTrabajadas;
+    float costoHora;
+    float sueldo;
+    printf("Ingrese las horas trabajadas por el empleado: ");
+    scanf(" %i", &horasTrabajadas);
+    printf("Ingrese el pago por hora: ");
+    scanf("%f", &costoHora);
+    sueldo = horasTrabajadas*costoHora;
+    printf("El sueldo total del operativo es ");
+    printf("%f", sueldo);
+    getchar();
+    return 0;
+}*/
