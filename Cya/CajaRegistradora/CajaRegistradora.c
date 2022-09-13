08e65fe8e0c2547d8503dc968dcb179bfdf74490
JohsSalinas <johssalinas@wposs.com>
2022-09-13 16:29:44 -0500

    Caja registradora: Funcionaaaa, pero falta validar pendejaditas

--- Cya/CajaRegistradora/CajaRegistradora.c
+++ Cya/CajaRegistradora/CajaRegistradora.c
@@ -3,7 +3,9 @@
 int main(){
     ListaProducto listaProducto = crearListaProducto();
     ListaVenta  listaVenta = crearListaVenta();
-    int opcion, continuar,bandera =1;
+    ListaProducto otrosProductos = crearListaProducto();
+    struct NodoProducto producto;
+    int opcion, continuar,bandera =1,cantProductos=0,otraOpcion;
     char comprador[30];
     //Funcion para calcular la fecha actual
     time_t t=time(NULL);
@@ -11,23 +13,39 @@ int main(){
     char fechayhora[100];
     strftime(fechayhora, 100, "%d/%m/%Y", tm);
 
-    agregarProducto(&listaProducto,"papa",1000,15);
-    agregarProducto(&listaProducto,"yuca",2000,25);
+    /*strcpy(producto.nombre,"yuca");
+    producto.cantidad = 25;
+    producto.precio = 2000;
+    agregarProducto(&listaProducto,producto);
+
+    strcpy(producto.nombre,"papa");
+    producto.cantidad = 15;
+    producto.precio = 1000;
+
+    agregarProducto(&listaProducto,producto);
     imprimirProducto(listaProducto);
-    struct Productos producto;
+
+
+    strcpy(comprador,"johs");
     strcpy(producto.nombre,"yuca");
     producto.cantidad = 5;
-    registrarVenta(&listaVenta,&listaProducto,fechayhora,"Yop",producto);
+    agregarProducto(&otrosProductos,producto);
     strcpy(producto.nombre,"papa");
     producto.cantidad = 4;
-    registrarVenta(&listaVenta,&listaProducto,fechayhora,"Yop",producto);
-    strcpy(producto.nombre,"papa");
+    agregarProducto(&otrosProductos,producto);
+    cantProductos=2;
+    registrarVenta(&listaVenta,&listaProducto,&otrosProductos,fechayhora,comprador, cantProductos);
+    strcpy(producto.nombre,"yuca");
+    strcpy(comprador,"brayan");
     producto.cantidad = 3;
-    registrarVenta(&listaVenta,&listaProducto,fechayhora,"brayan",producto);
-    inventario(listaVenta);
-    //imprimirProducto(listaProducto);
+    agregarProducto(&otrosProductos,producto);
+    cantProductos=1;
+    registrarVenta(&listaVenta,&listaProducto,&otrosProductos,fechayhora,comprador, cantProductos);
+    listaVentas(listaVenta);
+    imprimirProducto(listaProducto);*/
 
     while (bandera !=0) {
+        system("clear");
         printf("\n\tCaja resgistradora\n");
         printf("Escoja una opcion: \n");
         printf("1) Registrar venta\n");
@@ -49,16 +67,45 @@ int main(){
                     printf("cantidad: ");
                     fflush(stdin);
                     scanf("%d", &producto.cantidad);
-                    registrarVenta(&listaVenta,&listaProducto,fechayhora,comprador,producto);
                     printf("Desea agregar otro producto? 1)SI 2)NO");
                     scanf("%d", &continuar);
+                    cantProductos += 1;
+                    agregarProducto(&otrosProductos,producto);
                 } while (continuar == 1);
+                    registrarVenta(&listaVenta,&listaProducto,&otrosProductos,fechayhora,comprador, cantProductos);
                 printf("Se agrego correctamente\n");
+                cantProductos=0;
                 break;
             case 2:
+                printf("Escoja una opcion\n");
+                printf("1) Crear Producto\n");
+                printf("2) Listar Productos\n");
+                printf("3) Regresar\n");
+                fflush(stdin);
+                scanf("%d", &otraOpcion);
+                switch (otraOpcion) {
+                    case 1:
+                        printf("\nNombre del producto: ");
+                        fflush(stdin);
+                        gets(producto.nombre);
+                        printf("Precio: ");
+                        fflush(stdin);
+                        scanf("%d", &producto.precio);
+                        printf("Cantidad: ");
+                        fflush(stdin);
+                        scanf("%d", &producto.cantidad);
+                        agregarProducto(&listaProducto,producto);
+                        break;
+                    case 2:
+                        imprimirProducto(listaProducto);
+                        break;
+                    case 3:
+                        system("clear");
+                        break;
+                }
                 break;
             case 3:
-                inventario(listaVenta);
+                listaVentas(listaVenta);
                 break;
             case 0:
                 printf("Gracias por usar la caja registradora\n");
--- Cya/CajaRegistradora/CajaRegistradora.h
+++ Cya/CajaRegistradora/CajaRegistradora.h
@@ -3,35 +3,29 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct Productos{
+typedef struct NodoProducto{
     int idProducto;
     char nombre[30];
     int precio;
     int cantidad;
-}Productos;
-typedef struct Venta{
-    int idVenta;
-    char fecha[20];
-    char comprador[30];
-    int cantidadVenta;
-    Productos productos;
-}Venta;
-typedef struct NodoVenta{
-    Venta venta;
-    struct NodoVenta* next;
-}NodoVenta;
-typedef struct NodoProducto{
-    Productos productos;
     struct NodoProducto* sig;
 }NodoProducto;
-typedef struct ListaVenta{
-    struct NodoVenta* head;
-    int tamanoV;
-}ListaVenta;
 typedef struct ListaProducto{
     struct NodoProducto* primer;
     int tamanoP;
 }ListaProducto;
+typedef struct NodoVenta{
+    int idVenta;
+    char fecha[20];
+    char comprador[30];
+    int cantidadVenta;
+    struct ListaProducto* listaProducto;
+    struct NodoVenta* next;
+}NodoVenta;
+typedef struct ListaVenta{
+    struct NodoVenta* head;
+    int tamanoV;
+}ListaVenta;
 
 ListaVenta crearListaVenta(){
     ListaVenta listaVenta;
@@ -47,17 +41,17 @@ ListaProducto crearListaProducto() {
     return listaProducto;
 }
 
-void agregarProducto(ListaProducto *listaProducto, char nombre[], int precio, int cantidad){
+void agregarProducto(ListaProducto *listaProducto, struct NodoProducto producto){
     NodoProducto *temporal,*nodoProducto = NULL;
     nodoProducto = (NodoProducto*) calloc(1,sizeof (NodoProducto));
 
-    strcpy(nodoProducto->productos.nombre, nombre);
-    nodoProducto->productos.precio = precio;
-    nodoProducto->productos.cantidad = cantidad;
+    strcpy(nodoProducto->nombre, producto.nombre);
+    nodoProducto->precio = producto.precio;
+    nodoProducto->cantidad = producto.cantidad;
     int tamano = 0;
     tamano = listaProducto->tamanoP++;
-    nodoProducto->productos.idProducto = tamano;
-        printf("Ya se agrg%c el producto\n\n", 162);
+    nodoProducto->idProducto = tamano;
+        /*printf("Ya se agrg%c el producto\n\n", 162);*/
         if (listaProducto->primer == NULL)
             listaProducto->primer = nodoProducto;
         else {
@@ -69,46 +63,58 @@ void agregarProducto(ListaProducto *listaProducto, char nombre[], int precio, in
 
 void imprimirProducto(ListaProducto listaProducto){
     if (listaProducto.primer == NULL)
-        printf("La lista esta vacia\n");
+        printf("No hay productos\n");
     else{
         int tamano = listaProducto.tamanoP;
         printf("Los productos son: \n");
         NodoProducto *current = listaProducto.primer;
         while (tamano>0){
-            printf("id: %d\n",current->productos.idProducto);
-            printf("Nombre: %s\n",current->productos.nombre);
-            printf("Precio: %d\n",current->productos.precio);
-            printf("Cantidad: %d\n",current->productos.cantidad);
+            printf("\nid: %d\n",current->idProducto);
+            printf("Nombre: %s\n",current->nombre);
+            printf("Precio: %d\n",current->precio);
+            printf("Cantidad: %d\n",current->cantidad);
             current = current->sig;
             tamano--;
         }
     }
 }
 
-void registrarVenta(ListaVenta *listaVenta, ListaProducto  *listaProducto,char fecha[], char comprador[], struct Productos pro){
+void registrarVenta(ListaVenta *listaVenta, ListaProducto *listaProducto, ListaProducto  *otrosProductos, char fecha[], char comprador[], int cantProductos){
     NodoVenta *nodoVenta = NULL, *temporal= NULL;
+    struct NodoProducto producto;
     nodoVenta = (NodoVenta*) calloc(1,sizeof (NodoVenta));
+    nodoVenta->listaProducto = (struct ListaProducto *) malloc(sizeof(nodoVenta));
     NodoProducto *current = NULL;
+    NodoProducto *nodoProducto = NULL;
+    nodoProducto = otrosProductos->primer;
     current = listaProducto->primer;
-    int tamanoProducto = listaProducto->tamanoP;
+    nodoVenta->listaProducto->primer = NULL;
+    nodoVenta->listaProducto->tamanoP = 0;
+    int tamanoProducto = 0;
+    tamanoProducto = otrosProductos->tamanoP;
     while (tamanoProducto > 0) {
-            if (strcmp(pro.nombre,current->productos.nombre)==0) {
-                if (pro.cantidad < current->productos.cantidad) {
-                    strcpy(nodoVenta->venta.fecha,fecha);
-                    strcpy(nodoVenta->venta.comprador,comprador);
-                    strcpy(pro.nombre, current->productos.nombre);
-                    current->productos.cantidad -= pro.cantidad;
-                    pro.precio = current->productos.precio;
-                    pro.idProducto = current->productos.idProducto;
-                    strcpy(nodoVenta->venta.productos.nombre, pro.nombre);
-                    nodoVenta->venta.idVenta = listaVenta->tamanoV+1;
-                    nodoVenta->venta.cantidadVenta = pro.cantidad;
-                    nodoVenta->venta.productos.precio = pro.precio;
-                    nodoVenta->venta.productos.idProducto = pro.idProducto;
-                    nodoVenta->venta.productos.cantidad = pro.cantidad;
+        while (cantProductos>0) {
+            if (strcmp(nodoProducto->nombre,current->nombre)==0) {
+                if (nodoProducto->cantidad < current->cantidad) {
+                    strcpy(nodoVenta->fecha,fecha);
+                    strcpy(nodoVenta->comprador,comprador);
+                    strcpy(nodoProducto->nombre, current->nombre);
+                    current->cantidad -= nodoProducto->cantidad;
+                    nodoProducto->precio = current->precio;
+                    nodoProducto->idProducto = current->idProducto;
+                    nodoVenta->idVenta = listaVenta->tamanoV + 1;
+                    nodoVenta->cantidadVenta = nodoProducto->cantidad;
+                    strcpy(producto.nombre, nodoProducto->nombre);
+                    producto.precio = nodoProducto->precio;
+                    producto.idProducto = nodoProducto->idProducto;
+                    producto.cantidad = nodoProducto->cantidad;
+                    agregarProducto(nodoVenta->listaProducto, producto);
+                    cantProductos--;
+                    nodoProducto = nodoProducto->sig;
+                    current = current->sig;
                 }
-            }
-            current = current->sig;
+            }else current = current->sig;
+        }
             tamanoProducto--;
     }
     if (listaVenta->head == NULL)
@@ -119,40 +125,34 @@ void registrarVenta(ListaVenta *listaVenta, ListaProducto  *listaProducto,char f
         listaVenta->head->next = temporal;
     }
     listaVenta->tamanoV++;
+    otrosProductos->primer=NULL;
+    otrosProductos->tamanoP=0;
 }
 
-void inventario(ListaVenta listaVenta){
+void listaVentas(ListaVenta listaVenta){
     if (listaVenta.head == NULL)
         printf("No hay ventas\n");
     else {
-        int tamano = listaVenta.tamanoV, bandera =1;
         printf("\nLas Ventas son: \n\n");
-        NodoVenta *current = listaVenta.head, *temporal = current;
-        while (bandera !=0) {
-            printf("id de la venta: %d\n", current->venta.idVenta);
-            printf("Comprador: %s\n", current->venta.comprador);
-            printf("Fecha: %s\n", current->venta.fecha);
-            if(strcmp(temporal->venta.comprador,temporal->next->venta.comprador)==0) {
-                while (tamano > 0 && strcmp(temporal->venta.comprador,temporal->next->venta.comprador)==0) {
-                    printf("\nid del producto: %d\n", temporal->venta.productos.idProducto);
-                    printf("Nombre del producto: %s\n", temporal->venta.productos.nombre);
-                    printf("Precio: %d\n", temporal->venta.productos.precio);
-                    printf("Cantidad: %d\n", temporal->venta.productos.cantidad);
-                    tamano--;
-                    temporal = temporal->next;
-                }
-            }else if(strcmp(temporal->venta.comprador,temporal->next->venta.comprador)==1) {
-                while (tamano > 0 && strcmp(temporal->venta.comprador, temporal->next->venta.comprador) == 1) {
-                    printf("\nid del producto: %d\n", temporal->venta.productos.idProducto);
-                    printf("Nombre del producto: %s\n", temporal->venta.productos.nombre);
-                    printf("Precio: %d\n", temporal->venta.productos.precio);
-                    printf("Cantidad: %d\n", temporal->venta.productos.cantidad);
-                    tamano--;
-                    temporal = temporal->next;
-                }
+        NodoVenta *current = listaVenta.head;
+        int tamanoVenta = listaVenta.tamanoV;
+        while (tamanoVenta>0) {
+            int tamanoProducto = current->listaProducto->tamanoP;
+            NodoProducto *temporal = current->listaProducto->primer;
+            printf("id de la venta: %d\n", current->idVenta);
+            printf("Comprador: %s\n", current->comprador);
+            printf("Fecha: %s\n", current->fecha);
+            while (tamanoProducto>0){
+                printf("\nid del prducto: %d\n", temporal->idProducto);
+                printf("Nombre del producto: %s\n", temporal->nombre);
+                printf("Cantidad: %d\n", temporal->cantidad);
+                printf("Precio: %d\n", temporal->precio);
+                temporal = temporal->sig;
+                tamanoProducto--;
             }
-            printf("\n*************************************************\n");
-            bandera=0;
+            current = current->next;
+            tamanoVenta--;
+            printf("\n********************************************\n");
         }
     }
 }
